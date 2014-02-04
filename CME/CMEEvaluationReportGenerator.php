5c3bb5724c21185740799f949a91b6f823546b5e
Michael Gauthier <mike@silverorange.com>
2014-02-04 14:39:45 -0400

    Add quiz and evaluation report generators.

---
+++ CME/CMEEvaluationReportGenerator.php
@@ -0,0 +1,643 @@
+<?php
+
+require_once 'Swat/SwatDate.php';
+require_once 'SwatDB/SwatDB.php';
+require_once 'Site/SiteApplication.php';
+require_once 'Inquisition/dataobjects/InquisitionResponseWrapper.php';
+require_once 'CME/dataobjects/CMECreditType.php';
+require_once 'CME/dataobjects/EvaluationWrapper.php';
+require_once 'CME/dataobjects/EvaluationReport.php';
+
+/**
+ * @package   CME
+ * @copyright 2011-2014 silverorange
+ */
+abstract class CMEEvaluationReportGenerator
+{
+	// {{{ protected properties
+
+	/**
+	 * @var SwatDate
+	 */
+	protected $start_date;
+
+	/**
+	 * @var SwatDate
+	 */
+	protected $end_date;
+
+	/**
+	 * @var CMECreditType
+	 */
+	protected $credit_type;
+
+	/**
+	 * @var SiteApplication
+	 */
+	protected $app;
+
+	// }}}
+	// {{{ public function __construct()
+
+	public function __construct(SiteApplication $app,
+		CMECreditType $credit_type, $year, $quarter)
+	{
+		$this->app = $app;
+		$this->credit_type = $credit_type;
+
+		$start_month = ((intval($quarter) - 1) * 3) + 1;
+
+		$this->start_date = new SwatDate();
+		$this->start_date->setTime(0, 0, 0);
+		$this->start_date->setDate($year, $start_month, 1);
+		$this->start_date->setTZ($this->app->default_time_zone);
+
+		$this->end_date = clone $this->start_date;
+		$this->end_date->addMonths(3);
+	}
+
+	// }}}
+
+	// data retrieval methods
+	// {{{ protected function getResponses()
+
+	protected function getResponses()
+	{
+		$sql = sprintf(
+			'select * from InquisitionResponse
+			where complete_date is not null
+				and convertTZ(complete_date, %1$s) >= %2$s
+				and convertTZ(complete_date, %1$s) < %3$s
+				and inquisition in (
+					select evaluation from CMECredit where credit_type = %4$s
+				) and account in (
+					select id from Account where Account.delete_date is null
+				)',
+			$this->app->db->quote($this->app->config->date->time_zone, 'text'),
+			$this->app->db->quote($this->start_date->getDate(), 'date'),
+			$this->app->db->quote($this->end_date->getDate(), 'date'),
+			$this->app->db->quote($this->credit_type->id, 'integer')
+		);
+
+		$responses = SwatDB::query(
+			$this->app->db,
+			$sql,
+			'InquisitionResponseWrapper'
+		);
+
+		// efficiently load response values
+		$values = $responses->loadAllSubRecordsets(
+			'values',
+			SwatDBClassMap::get('InquisitionResponseValueWrapper'),
+			'InquisitionResponseValue',
+			'response'
+		);
+
+		// efficiently load response value question bindings
+		$question_binding_sql =
+			'select * from InquisitionInquisitionQuestionBinding
+			where id in (%s)';
+
+		$question_bindings = $values->loadAllSubDataObjects(
+			'question_binding',
+			$this->app->db,
+			$question_binding_sql,
+			SwatDBClassMap::get('InquisitionInquisitionQuestionBindingWrapper')
+		);
+
+		// and questions
+		$question_sql = 'select * from InquisitionQuestion where id in (%s)';
+		$questions = $question_bindings->loadAllSubDataObjects(
+			'question',
+			$this->app->db,
+			$question_sql,
+			SwatDBClassMap::get('InquisitionQuestionWrapper')
+		);
+
+		// efficiently load evaluations
+		$evaluation_sql = 'select * from Inquisition where id in (%s)';
+		$evaluations = $responses->loadAllSubDataObjects(
+			'inquisition',
+			$this->app->db,
+			$evaluation_sql,
+			'EvaluationWrapper'
+		);
+
+		// efficiently load question bindings
+		$wrapper = SwatDBClassMap::get(
+			'InquisitionInquisitionQuestionBindingWrapper'
+		);
+
+		$sql = sprintf(
+			'select * from InquisitionInquisitionQuestionBinding
+			where inquisition in (%s)
+			order by inquisition, displayorder',
+			$this->app->db->implodeArray($evaluations->getIndexes(), 'integer')
+		);
+
+		$question_bindings = SwatDB::query($this->app->db, $sql, $wrapper);
+		$evaluations->attachSubRecordset(
+			'question_bindings',
+			$wrapper,
+			'inquisition',
+			$question_bindings
+		);
+
+		// efficiently load questions
+		$question_sql = 'select * from InquisitionQuestion where id in (%s)';
+		$questions = $question_bindings->loadAllSubDataObjects(
+			'question',
+			$this->app->db,
+			$question_sql,
+			SwatDBClassMap::get('InquisitionQuestionWrapper')
+		);
+
+		// efficiently evaluation question options
+		$options = $questions->loadAllSubRecordsets(
+			'options',
+			SwatDBClassMap::get('InquisitionQuestionOptionWrapper'),
+			'InquisitionQuestionOption',
+			'question',
+			'',
+			'displayorder'
+		);
+
+		$response_array = array();
+		foreach ($responses as $response) {
+			// filter out responses for evaluations with no questions
+			if (count($response->inquisition->question_bindings) > 0) {
+				$response_array[] = $response;
+			}
+		}
+
+		return $response_array;
+	}
+
+	// }}}
+
+	// output methods
+	// {{{ public function saveFile()
+
+	public function saveFile($filename)
+	{
+		if (!file_exists(dirname($filename))) {
+			mkdir(dirname($filename), 0770, true);
+		}
+
+		$this->initDOMPDF();
+
+		ob_start();
+		$this->display();
+		$xhtml = ob_get_clean();
+
+		$dompdf = new DOMPDF();
+		$dompdf->load_html($xhtml);
+		$dompdf->render();
+
+		file_put_contents($filename, $dompdf->output());
+	}
+
+	// }}}
+	// {{{ protected function initDOMPDF()
+
+	protected function initDOMPDF()
+	{
+		if (!class_exists('DOMPDF')) {
+			define('DOMPDF_ENABLE_PHP', true);
+			include_once 'dompdf/dompdf_config.inc.php';
+		}
+	}
+
+	// }}}
+
+	// report display methods
+	// {{{ abstract protected function getTitle()
+
+	abstract protected function getTitle();
+
+	// }}}
+	// {{{ protected function display()
+
+	protected function display()
+	{
+		echo '<!DOCTYPE html>';
+		echo '<html>';
+		echo '<head>';
+
+		$title = new SwatHtmlTag('title');
+		$title->setContent($this->getTitle());
+		$title->display();
+
+		$this->displayStyles();
+
+		echo '</head>';
+		echo '<body>';
+
+		$this->displayFooter();
+
+		$this->displayEvaluation($this->getResponses());
+
+		echo '</body>';
+		echo '</html>';
+	}
+
+	// }}}
+	// {{{ protected function displayFooter()
+
+	protected function displayFooter()
+	{
+		$script_tag = new SwatHtmlTag('script');
+		$script_tag->type = 'text/php';
+		$script_tag->setContent('
+			// <![CDATA[
+			if (isset($pdf)) {
+				$footer = $pdf->open_object();
+
+				$font = Font_Metrics::get_font("arial");
+				if (!$font) {
+					$font = Font_Metrics::get_font("helvetica");
+				}
+				if (!$font) {
+					$font = Font_Metrics::get_font("sans-serif");
+				}
+
+				$black        = array(0, 0, 0);
+				$size         = 9;
+				$metrics_text = "Page 1 of 1";
+
+				if ($PAGE_NUM > 9) {
+					$metrics_text.= "0";
+				}
+				if ($PAGE_NUM > 99) {
+					$metrics_text.= "0";
+				}
+				if ($PAGE_COUNT > 9) {
+					$metrics_text.= "0";
+				}
+				if ($PAGE_COUNT > 99) {
+					$metrics_text.= "0";
+				}
+
+				$text        = "Page {PAGE_NUM} of {PAGE_COUNT}";
+
+				$width = Font_Metrics::get_text_width(
+					$metrics_text,
+					$font,
+					$size
+				);
+
+				$height      = Font_Metrics::get_font_height($font, $size);
+				$margin      = 54;
+				$page_width  = $pdf->get_width();
+				$page_height = $pdf->get_height();
+
+				$pdf->line(
+					$margin,
+					$page_height - $margin - $height - 12,
+					$page_width - $margin,
+					$page_height - $margin - $height - 12,
+					$black,
+					1
+				);
+
+				$x = ($page_width - $width) / 2;
+				$y = $page_height - $margin - $height;
+
+				$pdf->page_text($x, $y, $text, $font, $size, $black);
+
+				$pdf->close_object();
+				$pdf->add_object($footer, "all");
+			}
+			// ]]>
+		', 'text/xml');
+		$script_tag->display();
+	}
+
+	// }}}
+	// {{{ protected function displayStyles()
+
+	protected function displayStyles()
+	{
+		echo '<style>';
+		echo <<<STYLESHEET
+
+body {
+	font-size: 11pt;
+	font-family: Arial, Helvetica, sans-serif;
+}
+
+@page {
+	margin: 0.75in;
+}
+
+.page {
+	page-break-after: always;
+}
+
+h1, h2 {
+	text-align: center;
+	font-size: 18pt;
+	margin: 0 0 0.25in 0;
+}
+
+h2 {
+	font-size: 16pt;
+}
+
+h3 {
+	margin: 0 0 0.02in 0;
+	font-size: 13pt;
+}
+
+p, ul {
+	margin: 0;
+	padding: 0 0 0.2in 0;
+}
+
+ul {
+	padding: 0 0 0 2em;
+	margin-before: 0;
+	margin-after: 0;
+	padding-start: 0;
+	-moz-margin-before: 0;
+	-moz-margin-after: 0;
+	-moz-padding-start: 0;
+	-webkit-margin-before: 0;
+	-webkit-margin-after: 0;
+	-webkit-padding-start: 0;
+
+}
+
+li, ul {
+	list-style-type: none;
+}
+
+li {
+	margin: 0;
+	padding-top: 0.15in;
+}
+
+ul ul li, ul ul {
+	list-style-type: disc;
+}
+
+.question {
+	page-break-inside: avoid;
+	padding-bottom: 0.4in;
+}
+
+.question p {
+	margin: 0;
+	padding: 0;
+}
+
+.bar {
+	padding: 0.05in 0;
+}
+
+.bar-incomplete {
+	background: #ddd;
+	border-bottom: 0.02in solid #ddd;
+	width: 5in;
+	height: 0.1in;
+}
+
+.bar-complete {
+	background: #46c;
+	border-bottom: 0.02in solid #239;
+	height: 0.1in;
+}
+
+STYLESHEET;
+
+		echo '</style>';
+	}
+
+	// }}}
+	// {{{ protected function displayEvaluation()
+
+	protected function displayEvaluation(array $responses)
+	{
+		echo '<div class="page">';
+
+		$this->displayTitle();
+
+		$response_values_by_bodytext = array();
+		$questions = array();
+
+		foreach ($responses as $response) {
+			foreach ($response->values as $response_value) {
+				$question = $response_value->question_binding->question;
+				$bodytext = $question->bodytext;
+				$bodytext = strip_tags($bodytext);
+				$bodytext = str_replace('&nbsp;', '', $bodytext);
+
+				if (!isset($questions[$bodytext])) {
+					$questions[$bodytext] = $question;
+				}
+
+				if (!isset($response_values_by_bodytext[$bodytext])) {
+					$response_values_by_bodytext[$bodytext] = array();
+				}
+
+				$response_values_by_bodytext[$bodytext][] = $response_value;
+			}
+		}
+
+		// sort the responses by the alphabetical order of their questions
+		ksort($response_values_by_bodytext, SORT_STRING);
+
+		$index = 1;
+		foreach ($response_values_by_bodytext as $bodytext => $response_values) {
+			$question = $questions[$bodytext];
+
+			if ($question->question_type !== 100) {
+				$this->displayQuestion($question, $index++, $response_values);
+			}
+		}
+
+		echo '</div>';
+	}
+
+	// }}}
+	// {{{ protected function displayTitle()
+
+	protected function displayTitle()
+	{
+		$end_date = clone $this->end_date;
+		$end_date->subtractMonths(1);
+
+		printf(
+			'<h1>Program Evaluation Report for %s to %s</h1>',
+			$this->start_date->formatLikeIntl('MMMM yyyy'),
+			$end_date->formatLikeIntl('MMMM yyyy')
+		);
+	}
+
+	// }}}
+	// {{{ protected function displayQuestion()
+
+	protected function displayQuestion(InquisitionQuestion $question, $index,
+		array $response_values)
+	{
+		echo  '<div class="question">';
+
+		printf('<h3>Question %s</h3>', $index);
+
+		switch ($question->question_type) {
+		case InquisitionQuestion::TYPE_RADIO_LIST:
+			$this->displayRadioListQuestion($question, $response_values);
+			break;
+		case InquisitionQuestion::TYPE_FLYDOWN:
+			$this->displayFlydownQuestion($question, $response_values);
+			break;
+		case InquisitionQuestion::TYPE_RADIO_ENTRY:
+			$this->displayRadioEntryQuestion($question, $response_values);
+			break;
+		case InquisitionQuestion::TYPE_TEXT:
+			$this->displayTextQuestion($question, $response_values);
+			break;
+		}
+
+		echo '</div>';
+	}
+
+	// }}}
+	// {{{ protected function displayRadioEntryQuestion()
+
+	protected function displayRadioEntryQuestion(
+		InquisitionQuestion $question, array $response_values)
+	{
+		$locale = SwatI18NLocale::get();
+
+		echo $question->bodytext;
+
+		echo '<ul>';
+
+		$option_counts = array();
+		$option_values = array();
+		$total_count = 0;
+
+		if (count($response_values) > 0) {
+			foreach ($response_values as $value) {
+				$option_text = $value->question_option->title;
+				if (!isset($option_counts[$option_text])) {
+					$option_counts[$option_text] = 0;
+				}
+				$option_counts[$option_text]++;
+				if ($value->text_value != '') {
+					if (!isset($option_values[$option_text])) {
+						$option_values[$option_text] = array();
+					}
+					$option_values[$option_text][] = $value->text_value;
+				}
+				$total_count++;
+			}
+			foreach ($question->options as $option) {
+				if (isset($option_counts[$option->title])) {
+					$percent = $option_counts[$option->title] / $total_count;
+					$li_tag = new SwatHtmlTag('li');
+					$li_tag->setContent(
+						sprintf(
+							'%s - %s%% (%s)',
+							$option->title,
+							$locale->formatNumber(round($percent * 1000) / 10),
+							$locale->formatNumber(
+								$option_counts[$option->title]
+							)
+						)
+					);
+					$li_tag->open();
+					$li_tag->displayContent();
+					$this->displayBar($percent);
+					if (isset($option_values[$option->title])) {
+						echo '<ul>';
+						foreach ($option_values[$option->title] as $text) {
+							$li_tag = new SwatHtmlTag('li');
+							$li_tag->setContent($text);
+							$li_tag->display();
+						}
+						echo '</ul>';
+					}
+					$li_tag->close();
+				}
+			}
+		} else {
+			echo '<li>There were no responses for this question.</li>';
+		}
+
+		echo '</ul>';
+	}
+
+	// }}}
+	// {{{ protected function displayRadioListQuestion()
+
+	protected function displayRadioListQuestion(
+		InquisitionQuestion $question, array $response_values)
+	{
+		$this->displayRadioEntryQuestion($question, $response_values);
+	}
+
+	// }}}
+	// {{{ protected function displayFlydownQuestion()
+
+	protected function displayFlydownQuestion(
+		InquisitionQuestion $question, array $response_values)
+	{
+		$this->displayRadioListQuestion($question, $response_values);
+	}
+
+	// }}}
+	// {{{ protected function displayTextQuestion()
+
+	protected function displayTextQuestion(
+		InquisitionQuestion $question, array $response_values)
+	{
+		// Don't display the response text for now. We require an answer for
+		// every text question. As a consequence it means we'll end up
+		// displaying A LOT of text. We don't want to do that right now.
+		echo $question->bodytext;
+		echo '<p>Comments are not listed in this report.</p>';
+
+		/*
+		if (count($response_values) > 0) {
+			echo '<p>The following answers were provided:</p>';
+			echo '<ul>';
+			$count = 0;
+			foreach ($response_values as $value) {
+				$li_tag = new SwatHtmlTag('li');
+				$li_tag->setContent($value->text_value);
+				$li_tag->display();
+				if ($count++ > 25) break;
+			}
+			echo '</ul>';
+		} else {
+			echo '<p>There were no responses for this question.</p>';
+		}
+		*/
+	}
+
+	// }}}
+	// {{{ protected function displayBar()
+
+	protected function displayBar($percent)
+	{
+		$percent = round($percent * 1000) / 10;
+
+		echo '<div class="bar">';
+		echo '<div class="bar-incomplete">';
+
+		$bar_complete = new SwatHtmlTag('div');
+		$bar_complete->class = 'bar-complete';
+		$bar_complete->setContent('');
+		$bar_complete->style = 'width: '.$percent.'%;';
+		$bar_complete->display();
+
+		echo '</div>';
+		echo '</div>';
+	}
+
+	// }}}
+}
+
+?>
---
+++ CME/CMEQuizReportGenerator.php
@@ -0,0 +1,464 @@
+<?php
+
+require_once 'Swat/SwatDate.php';
+require_once 'SwatDB/SwatDB.php';
+require_once 'Site/SiteApplication.php';
+require_once 'Inquisition/dataobjects/InquisitionResponseWrapper.php';
+require_once 'CME/dataobjects/CMECreditWrapper.php';
+require_once 'CME/dataobjects/CMECreditType.php';
+require_once 'CME/dataobjects/CMEQuizWrapper.php';
+
+/**
+ * @package   CME
+ * @copyright 2011-2014 silverorange
+ */
+abstract class CMEQuizReportGenerator
+{
+	// {{{ protected properties
+
+	/**
+	 * @var SwatDate
+	 */
+	protected $start_date;
+
+	/**
+	 * @var SwatDate
+	 */
+	protected $end_date;
+
+	/**
+	 * @var CMECreditType
+	 */
+	protected $credit_type;
+
+	/**
+	 * @var array
+	 */
+	protected $credits_by_quiz = array();
+
+	/**
+	 * @var SiteApplication
+	 */
+	protected $app;
+
+	// }}}
+	// {{{ public function __construct()
+
+	public function __construct(SiteApplication $app,
+		CMECreditType $credit_type, $year, $quarter)
+	{
+		$this->app = $app;
+		$this->credit_type = $credit_type;
+
+		$start_month = ((intval($quarter) - 1) * 3) + 1;
+
+		$this->start_date = new SwatDate();
+		$this->start_date->setTime(0, 0, 0);
+		$this->start_date->setDate($year, $start_month, 1);
+		$this->start_date->setTZ($this->app->default_time_zone);
+
+		$this->end_date = clone $this->start_date;
+		$this->end_date->addMonths(3);
+	}
+
+	// }}}
+
+	// data retrieval methods
+	// {{{ protected function getResponses()
+
+	protected function getResponses()
+	{
+		$sql = sprintf(
+			'select * from InquisitionResponse
+			where complete_date is not null
+				and reset_date is null
+				and convertTZ(complete_date, %1$s) >= %2$s
+				and convertTZ(complete_date, %1$s) < %3$s
+				and inquisition in (
+					select quiz from CMECredit where credit_type = %4$s
+				) and account in (
+					select id from Account where Account.delete_date is null
+				)',
+			$this->app->db->quote($this->app->config->date->time_zone, 'text'),
+			$this->app->db->quote($this->start_date->getDate(), 'date'),
+			$this->app->db->quote($this->end_date->getDate(), 'date'),
+			$this->app->db->quote($this->credit_type->id, 'integer')
+		);
+
+		$responses = SwatDB::query(
+			$this->app->db,
+			$sql,
+			SwatDBClassMap::get('InquisitionResponseWrapper')
+		);
+
+		// efficiently load accounts
+		$accounts = $this->loadAccounts($responses);
+
+		// load addresses
+		$addresses = $this->loadAddresses($accounts);
+
+		// efficiently load response values
+		$values = $responses->loadAllSubRecordsets(
+			'values',
+			SwatDBClassMap::get('InquisitionResponseValueWrapper'),
+			'InquisitionResponseValue',
+			'response'
+		);
+
+		// efficiently load response value question bindings
+		$question_binding_sql =
+			'select * from InquisitionInquisitionQuestionBinding
+			where id in (%s)';
+
+		$question_bindings = $values->loadAllSubDataObjects(
+			'question_binding',
+			$this->app->db,
+			$question_binding_sql,
+			SwatDBClassMap::get('InquisitionInquisitionQuestionBindingWrapper')
+		);
+
+		// efficiently load response value questions
+		$question_sql = 'select * from InquisitionQuestion where id in (%s)';
+		$questions = $question_bindings->loadAllSubDataObjects(
+			'question',
+			$this->app->db,
+			$question_sql,
+			SwatDBClassMap::get('InquisitionQuestionWrapper')
+		);
+
+		// efficiently load quizzes
+		$quiz_sql = 'select * from Inquisition where id in (%s)';
+		$quizzes = $responses->loadAllSubDataObjects(
+			'inquisition',
+			$this->app->db,
+			$quiz_sql,
+			'CMEQuizWrapper'
+		);
+
+		// efficiently load credits
+		$credits = SwatDB::query(
+			$this->app->db,
+			sprintf(
+				'select id, hours, quiz, episode
+					from CMECredit
+				where quiz in (%s)',
+				$this->app->db->implodeArray($quizzes->getIndexes(), 'integer')
+			),
+			'CMECreditWrapper'
+		);
+
+		$credits->attachSubDataObjects('quiz', $quizzes);
+
+		// index credits by quiz
+		foreach ($credits as $credit) {
+			$this->credits_by_quiz[$credit->quiz->id] = $credit;
+		}
+
+		// efficiently load question bindings
+		$wrapper = SwatDBClassMap::get(
+			'InquisitionInquisitionQuestionBindingWrapper'
+		);
+
+		$sql = sprintf(
+			'select * from InquisitionInquisitionQuestionBinding
+			where InquisitionInquisitionQuestionBinding.inquisition in (%s)
+			order by inquisition, displayorder',
+			$this->app->db->implodeArray($quizzes->getIndexes(), 'integer')
+		);
+
+		$question_bindings = SwatDB::query($this->app->db, $sql, $wrapper);
+		$quizzes->attachSubRecordset(
+			'question_bindings',
+			$wrapper,
+			'inquisition',
+			$question_bindings
+		);
+
+		// efficiently load questions
+		$sql = 'select * from InquisitionQuestion where id in (%s)';
+		$questions = $question_bindings->loadAllSubDataObjects(
+			'question',
+			$this->app->db,
+			$question_sql,
+			SwatDBClassMap::get('InquisitionQuestionWrapper')
+		);
+
+		$response_array = array();
+		foreach ($responses as $response) {
+			// filter out responses for quizzes with no questions
+			if (count($response->inquisition->question_bindings) > 0) {
+				$response_array[] = $response;
+			}
+		}
+
+		// sort responses
+		usort($response_array, array($this, 'compareResponse'));
+
+		// index by inquisition
+		$responses_by_inquisition = array();
+
+		foreach ($response_array as $response) {
+			$inquisition_id = $response->getInternalValue('inquisition');
+			if (!isset($responses_by_inquisition[$inquisition_id])) {
+				$responses_by_inquisition[$inquisition_id] = array();
+			}
+			$responses_by_inquisition[$inquisition_id][] = $response;
+		}
+
+		return $responses_by_inquisition;
+	}
+
+	// }}}
+	// {{{ protected function loadAccounts()
+
+	protected function loadAccounts(InquisitionResponseWrapper $responses)
+	{
+		// efficiently load accounts
+		$account_sql = 'select id, email, default_billing_address from Account
+			where id in (%s)';
+
+		$accounts = $responses->loadAllSubDataObjects(
+			'account',
+			$this->app->db,
+			$account_sql,
+			SwatDBClassMap::get('SiteAccountWrapper')
+		);
+
+		return $accounts;
+	}
+
+	// }}}
+	// {{{ protected function loadAccountAddresses()
+
+	protected function loadAccountAddresses(SiteAccountWrapper $accounts)
+	{
+		$addresses = $accounts->loadAllSubRecordsets(
+			'addresses',
+			SwatDBClassMap::get('StoreAccountAddressWrapper'),
+			'AccountAddress',
+			'account'
+		);
+
+		$provstate_sql = 'select * from Provstate where id in (%s)';
+		$addresses->loadAllSubDataObjects(
+			'provstate',
+			$this->app->db,
+			$provstate_sql,
+			SwatDBClassMap::get('StoreProvstateWrapper')
+		);
+
+		$country_sql = 'select * from Country where id in (%s)';
+		$addresses->loadAllSubDataObjects(
+			'country',
+			$this->app->db,
+			$country_sql,
+			SwatDBClassMap::get('StoreCountryWrapper'),
+			'text'
+		);
+
+		return $addresses;
+	}
+
+	// }}}
+	// {{{ protected function getCredit()
+
+	protected function getCredit(CMEQuiz $quiz)
+	{
+		return $this->credits_by_quiz[$quiz->id];
+	}
+
+	// }}}
+	// {{{ abstract protected function compareResponse()
+
+	abstract protected function compareResponse(
+		InquisitionResponse $a,
+		InquisitionResponse $b);
+
+	// }}}
+
+	// output methods
+	// {{{ public function saveFile()
+
+	public function saveFile($filename)
+	{
+		if (!file_exists(dirname($filename))) {
+			mkdir(dirname($filename), 0770, true);
+		}
+
+		$file = fopen($filename, 'w');
+		$this->display($file);
+		fclose($file);
+	}
+
+	// }}}
+
+	// report display methods
+	// {{{ protected function getHeaderRow()
+
+	protected function getHeaderRow()
+	{
+		return array(
+			'Last Name',
+			'First Name',
+			'Suffix',
+			'Email',
+			'Address',
+			'City',
+			'State / Province',
+			'ZIP / Postal Code',
+			'Country',
+			'Phone',
+			'Hours',
+			'Date Completed',
+		);
+	}
+
+	// }}}
+	// {{{ protected function getQuizResponseRow()
+
+	protected function getQuizResponseRow(InquisitionResponse $response)
+	{
+		$address = $response->account->getDefaultBillingAddress();
+
+		if ($address === null) {
+			$address = $response->account->addresses->getFirst();
+		}
+
+		if ($address === null) {
+			return;
+		}
+
+		$quiz   = $response->inquisition;
+		$credit = $this->getCredit($quiz);
+
+		$complete_date = clone $response->complete_date;
+		$complete_date->convertTZ($this->app->default_time_zone);
+
+		$address_lines = $this->formatLines($address);
+		$address_suffix = $this->formatSuffix($response->account, $address);
+		$address_provstate = $this->formatProvState($address);
+		$address_postal_code = $this->formatPostalCode($address);
+
+		return array(
+			$address->last_name,
+			$address->first_name,
+			$address_suffix,
+			$response->account->email,
+			$address_lines,
+			$address->city,
+			$address_provstate,
+			$address_postal_code,
+			$address->country->title,
+			$address->phone,
+			$credit->hours,
+			$complete_date->formatLikeIntl('MMMM dd, yyyy'),
+		);
+	}
+
+	// }}}
+	// {{{ protected function display()
+
+	protected function display($file)
+	{
+		$this->displayHeader($file);
+
+		$responses_by_inquisition = $this->getResponses();
+		foreach ($responses_by_inquisition as $responses) {
+			$this->displayQuizResponses($file, $responses);
+		}
+	}
+
+	// }}}
+	// {{{ protected function displayHeader()
+
+	protected function displayHeader($file)
+	{
+		fputcsv($file, $this->getHeaderRow());
+	}
+
+	// }}}
+	// {{{ protected function displayQuizResponses()
+
+	protected function displayQuizResponses($file, array $responses)
+	{
+		foreach ($responses as $response) {
+			if ($response->isPassed()) {
+				$this->displayQuizResponse($file, $response);
+			}
+		}
+	}
+
+	// }}}
+	// {{{ protected function displayQuizResponse()
+
+	protected function displayQuizResponse($file, InquisitionResponse $response)
+	{
+		fputcsv($file, $this->getQuizResponseRow($response));
+	}
+
+	// }}}
+	// {{{ protected function formatPostalCode()
+
+	protected function formatPostalCode(StoreAddress $address)
+	{
+		$postal_code = $address->postal_code;
+
+		switch ($address->country->id) {
+		case 'CA':
+			$postal_code = str_replace(array(' ', '-'), '', $postal_code);
+			$postal_code = strtoupper($postal_code);
+			break;
+
+		case 'US':
+			$matches = array();
+			$postal_code = trim($postal_code);
+			$zip_4 = '/([0-9]{5})[- ][0-9]{4}/u';
+			if (preg_match($zip_4, $postal_code, $matches) === 1) {
+				$postal_code = $matches[1];
+			}
+			break;
+		}
+
+		return $postal_code;
+	}
+
+	// }}}
+	// {{{ protected function formatSuffix()
+
+	protected function formatSuffix(Account $account, StoreAddress $address)
+	{
+		return $address->suffix;
+	}
+
+	// }}}
+	// {{{ protected function formatLines()
+
+	protected function formatLines(StoreAddress $address)
+	{
+		$address_lines = $address->line1;
+
+		if ($address->line2 != '') {
+			$address_lines.= ' '.$address->line2;
+		}
+
+		return $address_lines;
+	}
+
+	// }}}
+	// {{{ protected function formatProvState()
+
+	protected function formatProvState(StoreAddress $address)
+	{
+		$provstate = $address->provstate_other;
+
+		if ($address->provstate instanceof StoreProvState) {
+			$provstate = $address->provstate->abbreviation;
+		}
+
+		return $provstate;
+	}
+
+	// }}}
+}
+
+?>
