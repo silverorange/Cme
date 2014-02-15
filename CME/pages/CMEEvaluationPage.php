ef5d7ee06f8bea0ffd81c0f6ad9c0a4b80352c4f
Michael Gauthier <mike@silverorange.com>
2014-02-15 19:35:37 -0400

    Make abstract and fix copyright date.

--- CME/pages/CMEEvaluationPage.php
+++ CME/pages/CMEEvaluationPage.php
@@ -10,10 +10,10 @@ require_once 'CME/dataobjects/CMEEvaluationWrapper.php';
 
 /**
  * @package   CME
- * @copyright 2011-2043 silverorange
+ * @copyright 2011-2014 silverorange
  * @license   http://www.opensource.org/licenses/mit-license.html MIT License
  */
-class CMEEvaluationPage extends SiteDBEditPage
+abstract class CMEEvaluationPage extends SiteDBEditPage
 {
 	// {{{ protected properties
 
