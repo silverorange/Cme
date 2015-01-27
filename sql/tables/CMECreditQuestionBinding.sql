0c41a4e9add165b5ecef3832c69a3d28ae3b1075
Nick Burka <nick@silverorange.com>
2015-01-27 13:52:10 -0400

    Match the SQL to InquisitionInquisitionQuestionBinding

--- sql/tables/CMECreditQuestionBinding.sql
+++ sql/tables/CMECreditQuestionBinding.sql
@@ -1,7 +1,16 @@
+drop table CMECreditQuestionBinding;
+
 create table CMECreditQuestionBinding (
+	id serial,
 	credit integer not null references CMECredit(id) on delete cascade,
 	question integer not null references InquisitionQuestion(id) on delete cascade,
 	displayorder integer not null default 0,
 
-	primary key(credit, question)
+	primary key(id)
 );
+
+create index CMECreditQuestionBinding_credit_index on
+	CMECreditQuestionBinding(credit);
+
+create index CMECreditQuestionBinding_question_index on
+	CMECreditQuestionBinding(question);
