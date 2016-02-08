7071920fd9f877470298344a32d8555736f5d07f
GervaisdeM <gervais@demontbrun.com>
2016-02-08 11:29:47 -0400

    Prepare for release of 0.1.21 | package party

--- package.php
+++ package.php
@@ -39,7 +39,7 @@ PEAR::setErrorHandling(PEAR_ERROR_DIE);
 $api_version     = '0.1.0';
 $api_state       = 'beta';
 
-$release_version = '0.1.20';
+$release_version = '0.1.21';
 $release_state   = 'beta';
 $release_notes   = 'beta release';
 
